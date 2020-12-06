865660bc3b955718f52c50934c63f6d091b63343
yvikasofficial <yvikasofficial@gmail.com>
2020-12-06 23:34:01 +0530

    Added Path Vars and config for mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => console.log('Connected to DB!'));
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
