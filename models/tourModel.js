4d9e6dbedbc495c0c0b50529262c4c2aaca1b3c1
yvikasofficial <yvikasofficial@gmail.com>
2020-12-11 01:57:04 +0530

    Added Document Middleware for tourScheme

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     rating: {
       type: Number,
       default: 4.5,
@@ -67,6 +69,11 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// tourSchema.pre('save', function (next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
